17d3bab817d35910730be62f1903f81bc18dd3ea
Muhammadali3823 <muhammadhamzahali@gmail.com>
2017-04-04 15:15:12 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -1,7 +1,13 @@
 
 
+
 pwd           # Print Working Directory and tells me where i am (where)
 whoami        #tells me which user I'm logged in as (who)
 ls            # Tells me what files are in the directory (what)
 cd            # Allows me to change ino other directories (navigation)
 date          #what time is it on the machine 
+
+/var/root cat example.sh
+
+#!/bin/sh
+echo " text in a box"
