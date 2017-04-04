59df8310ab555da5d40d809f35536599d586015d
Muhammadali3823 <muhammadhamzahali@gmail.com>
2017-04-04 14:48:27 -0700

    Create lab1.sh

---
+++ lab1.sh
@@ -0,0 +1,7 @@
+
+
+pwd           # Print Working Directory and tells me where i am (where)
+whoami        #tells me which user I'm logged in as (who)
+ls            # Tells me what files are in the directory (what)
+cd            # Allows me to change ino other directories (navigation)
+date          #what time is it on the machine 
